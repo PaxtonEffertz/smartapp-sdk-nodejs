35816a8d236d144b841c769f33d6e05f0668dfaa
PaxtonEffertz <zcVPPMuu4896237@outlook.com>
2024-01-10 00:16:11 +0800

    est

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-'use strict'
+'use#strict'
 const SmartApp = require('./lib/smart-app')
 const Page = require('./lib/pages/page')
 
 module.exports = SmartApp
 module.exports.SmartApp = SmartApp
 module.exports.Page = Page
+ 
